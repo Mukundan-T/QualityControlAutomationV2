eeda4d9d573d5991da8b4716e9197fd13eb3fca6
libstudent <libstudent@union.edu>
2024-08-16 13:43:33 -0400

    added better docstrings for maintainability

--- Python Scripts/Spreadsheet_checks.py
+++ Python Scripts/Spreadsheet_checks.py
@@ -15,7 +15,9 @@ import time, sys, easygui, random, string, datetime, openpyxl, os
 
 
 """Print slow function to simulate typing
-    makes interfce more friendly but not necessarily needed
+    makes interfce more friendly but not necessarily needed. Typing speed changed by altering typing_speed to reflect words per minute
+Args:
+    String t: The string typed out
 """
 def print_slow(t):
     typing_speed = 60 #wpm
@@ -26,6 +28,10 @@ def print_slow(t):
     print ('')
 
 """Removes blank spaces added to spreadsheet during data entry
+Args:
+    List: String broken up into parts
+Returns:
+    List without blank spaces
 """
 def removeBlanks(lst):
     while '' in lst:
@@ -34,7 +40,6 @@ def removeBlanks(lst):
 
 
 """Opens an EasyGUI window to allow the user to select the file they want to parse
-
 Returns:
     String: filepath of the selected file
 """
@@ -42,6 +47,14 @@ def get_file():
     path = easygui.fileopenbox()
     return path
 
+
+"""Uses pandas to read excel document selected by the user
+Args:
+    String: Filepath decide dby the user in the selection process
+Returns:
+    Dict: Dataframes for each sheet in the file
+    List[String]:Sheetnames in the file
+"""
 def get_dataFrames(filepath):
     xl_file = pd.ExcelFile(filepath)
     dfs = pd.read_excel(xl_file, sheet_name=None)
@@ -50,6 +63,14 @@ def get_dataFrames(filepath):
     xl_file.close()
     return dfs,sheets
 
+"""Drops the uneccesary fields from the dataframe to make it easier to use
+    Can be expanded - only the fields needed by the program should be in the dataframe since we read the full df again before writing out
+Args:
+    Dict: dataframes for each sheet
+    Keys: keys for the dataframes dictionary - in this case sheet names
+Returns:
+    Dict: dataframes without the unecessary fields
+"""
 def clean_dataFrames(dfs, keys):
     for key in keys:
         dfs[key].drop(['documents','type','local_identifier','label (title)','creator_role','contributor_role','genre'], axis=1, inplace=True) #Can be taken out before final version, just helps to view simpler spreadsheet
@@ -58,6 +79,10 @@ def clean_dataFrames(dfs, keys):
 
 """Determines the correct prefix for the files by finding the most common from the file
     Means the script can be used for different collections
+Args:
+    List[String]: filenames used to determine most likely correct prefix
+Returns:
+    String: most likely prefix given all the entries
 """
 def find_file_prefix(filenames):
     filenameDict = {} #Dictionary containing the prefixes in the document and their count
@@ -73,6 +98,13 @@ def find_file_prefix(filenames):
 """Splits the Physical location into Box, Folder and, if available, Item
     Uses the prefix to reconstruct the filename
     Searches the folder to locate the files by the determined filenames
+Args:
+    Dataframe: current pandas dataframe being worked on
+    Array[String]: ReportMajor containing major errors to be exported to the report
+    Array[String]: ReportNoLocation exports rows with no physical location to the Report in Minor section
+    Array[String]: Problem_rows containing the filenames that do not match so they can be highlighted and added to the text report
+Returns:
+    Boolean: Used to determine if the process was successful or not
 """
 def check_location_filename(df, reportMajor, reportNoLocation, problem_rows):
     locations = df[['aspace_id','Physical Location','Filename']].copy()
@@ -127,6 +159,14 @@ def check_location_filename(df, reportMajor, reportNoLocation, problem_rows):
     return True
 
 
+"""Checks for duplicate filenames in the sheet
+Args:
+    Dataframe: current pandas dataframe being worked on
+    Array[String]: ReportMajor containing duplicate filenames be exported to the report
+    Array[String]: Problem_rows containing the dulicate filenames so they can be highlighted and added to the text report
+Returns:
+    Boolean: True or False may be used to verify the process was executed successfully - not yet implemented
+"""
 def check_duplicate_filenames(df, reportMajor, problem_rows):
     filenames = df["Filename"][df['Filename'].duplicated(keep=False)]
     for name in filenames:
@@ -135,12 +175,21 @@ def check_duplicate_filenames(df, reportMajor, problem_rows):
             reportMajor.append("Filename: " + name +" is duplicated")
     return True
 
+"""Coming soon
+"""
 def check_date_format(df, reportMajor, problem_rows):
 
     return True
 
-    
-def createReportText(sheetName,reportMajor, reportNoLocation, reportMinor):
+
+"""Creates the report text files containing errors
+Args:
+    String: sheetName denoting which sheet is currently being worked on - Box 5, Box 6 etc.
+    Array[String]: reportMajor containing the error messages ad filenames to be outputted in the report
+    Array[String]: reportNoLocation containing minor location errors for the report
+    Array[String]: reportMinor will be used for other errors that aren't in these categories
+"""   
+def createReportText(sheetName, reportMajor, reportNoLocation, reportMinor):
 
     newpath = (os.path.dirname(filepath) + "\\Reports\\")
     if not os.path.exists(newpath):
@@ -158,15 +207,30 @@ def createReportText(sheetName,reportMajor, reportNoLocation, reportMinor):
     outputFile.close()
 
 
+"""Resets the color of rows in the worksheet so they can be reassessed based on the progra's findings
+Args:
+    worksheet: current sheet opened as part of a Workbook with the openpyxl library
+    Int: i refering to the row index after opening the worksheet
+    Dict: the colors that are used by the program to highlight errors
+        This way we can avoid removing colorson the spreadsheet not caused by previous program runs
 
-def reset_color_rows(wb, ws, i, error_fills):
+"""
+def reset_color_rows(ws, i, error_fills):
     fill_reset = openpyxl.styles.PatternFill(fill_type=None)
     if ws.cell(row=i+2, column=1).fill.start_color.index in error_fills.values():
         for y in range(1, ws.max_column+1):
             ws.cell(row=i+2, column=y).fill = fill_reset
 
 
-
+"""Identifies the problem rows in the spreadsheet and highlights them depending on the error type
+Args:
+    String: sheetname - the current sheet being worked on
+    Array[String]: the current problem rows, identified by their filename
+    String: the type of error currently being added to the spreadsheet - this dictates highlight color
+    Boolean: informs the subroutine wether a color reset is required for the spreadsheet - runs when the first error highlight is being added
+Returns:
+    Boolean: depending on wether the process was successful or not. Fails usually occur due to the excel spreadsheet being open in another application
+"""
 def identify_problem_rows(sheetname, problem_rows, type, reset_rows):
 
     #type will be used to determine if the passed problem rows are filename, date format etc.
@@ -184,7 +248,7 @@ def identify_problem_rows(sheetname, problem_rows, type, reset_rows):
     for index, row in dt.iterrows():
 
         if reset_rows:
-            reset_color_rows(wb, ws, index, error_fills)
+            reset_color_rows(ws, index, error_fills)
 
         for p in problem_rows:
             #print(dt['Filename'][index])
@@ -205,6 +269,12 @@ def identify_problem_rows(sheetname, problem_rows, type, reset_rows):
     return True
 
 
+"""The sheet loop that checks each sheet for errors and creates the error reports
+    Contains error checking for the whole program - if any process is not successful and is unhandled an error message is produced
+Args:
+    Dict: dataframes - all the dataframes in the document split by sheet
+    Array[String]: the names of the sheets in the document
+"""
 def SheetLoop(dfs, sheets):
 
     for sheet in sheets:
@@ -225,7 +295,7 @@ def SheetLoop(dfs, sheets):
         if False not in sheet_success:
             createReportText(sheet, reportMajor, reportNoLocation, reportMinor)
             error_rate = round((len(reportMajor)/ (dfs[sheet].shape[0]) * 100) , 1)
-            print(sheet + " Completed.     Error Rate: " + str(round(error_rate,1)) + "%")
+            #print(sheet + " Completed.     Error Rate: " + str(round(error_rate,1)) + "%")
         else:
             break
 
@@ -235,7 +305,8 @@ def SheetLoop(dfs, sheets):
         tk.messagebox.showinfo("Success", "Success! Please check the excel file for issues")
 
 
-
+"""Begins the sheetloop that systematically checks each sheet
+"""
 def run_checks():
     global filepath
 
@@ -243,7 +314,7 @@ def run_checks():
         filepath = get_file()
 
         dfs, sheets = get_dataFrames(filepath)
-        print_slow("File loaded...")
+        #print_slow("File loaded...")
         dfs = clean_dataFrames(dfs, sheets)
 
         #check_location_filename(dfs['Box 6'], reportMajor, reportNoLocation) test
@@ -253,22 +324,7 @@ def run_checks():
 
 
     ### For Debugging purposes take out try except
-    """
-    filepath = get_file()
 
-    dfs, sheets = get_dataFrames(filepath)
-    print_slow("File loaded...")
-    dfs = clean_dataFrames(dfs, sheets)
-
-    SheetLoop(dfs, sheets)
-
-    """
-
-
-    
-
-def run_date_checks():
-    tk.messagebox.showinfo("Oops", "Under Construction")
 
 # run_name_checks()    
 
