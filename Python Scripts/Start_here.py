f9bf2281e535c3d3617eaa9b25a9d1da5b899c3e
libstudent <libstudent@union.edu>
2024-08-21 12:05:53 -0400

    changed around the User Interface

--- Python Scripts/Start_here.py
+++ Python Scripts/Start_here.py
@@ -11,21 +11,22 @@ Edited by:
 import Spreadsheet_checks
 import Quality_control
 
-import sys
-import qtstylish
-from PyQt5 import QtCore, QtGui
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGroupBox, QTextBrowser
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
+from File_finder import get_file
 
+import sys, os, qtstylish
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QPixmap, QPainter, QPen, QPainterPath
+from PyQt5.QtCore import Qt
+
+x
 class App(QWidget):
     def __init__(self):
         super().__init__()
         self.title = 'Menu'
         self.left = 20
         self.top = 80
-        self.width = 320
-        self.height = 200
+        self.width = 600
+        self.height = 400
         self.setStyleSheet(qtstylish.light())
         self.initUI()
   
@@ -35,24 +36,84 @@ class App(QWidget):
         self.setFixedSize(self.width, self.height)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
+        self.excel = QLabel(self)
+        self.excelPixmap = QPixmap(os.getcwd() + "\\Python Scripts\\excel.png")
+        self.smaller_pixmap = self.excelPixmap.scaled(24, 24, Qt.KeepAspectRatio)
+        self.excel.setPixmap(self.smaller_pixmap)   
+        self.excel.move(15,15)
+
+        Find_file = QPushButton('Search', self)
+        Find_file.setGeometry(480, 18, 100,25)
+
+
+
         Prelim = QPushButton('Preliminary Spreadsheet Checks', self)
-        Prelim.setGeometry(self.left, self.top - 60, self.width - 40, 30)
+        Prelim.setGeometry(self.left, self.top - 20, int(self.width/2 - 20), 30)
         Prelim.clicked.connect(Spreadsheet_checks.run_checks) #Needs to run name checks and date formatting checks (Spreadsheet_checks.py)
 
-        QualityControl = QPushButton('Quality Control', self)
-        QualityControl.setGeometry(self.left, self.top - 20, self.width - 40, 30)
-        #QualityControl.clicked.connect() #Needs to run from the Quality_control file
-
-        FinalChecks = QPushButton('Final Checks', self)
-        FinalChecks.setGeometry(self.left, self.top + 20, self.width -40, 30)
-        #FinalChecks.clicked.connect() #Don't know where this will connect yet
+        self.showTextbox("")
 
         self.show()
+
+
+    def showTextbox(self, text):
+        File_text = QTextEdit(text, self)
+        File_text.move(65,18)
+        File_text.resize(400, 25)
+        File_text.setReadOnly(True)
+
+            
+        #Prelim = QPushButton('Preliminary Spreadsheet Checks', self)
+        #Prelim.setGeometry(self.left, self.top - 60, self.width - 40, 30)
+        #Prelim.clicked.connect(Spreadsheet_checks.run_checks) #Needs to run name checks and date formatting checks (Spreadsheet_checks.py)
+
+        
+        
+        self.update()
+
+class shape():
+    def __init__(self,length,position,color):
+        self.length=length
+        self.position=position
+        self.color = color
+
+class rectangle(shape):
+    def paintEvent(self, event):
+
+        painter = QPainter(self)
+
+        painter.setPen(QPen(Qt.yellow,  7, Qt.DotLine))
+
+        painter.drawRect(10, 20, 100, 200)
+
+class square(shape):
+    def paintEvent(self, event):
+
+        painter = QPainter()
+
+        path = QPainterPath()
+
+        painter.begin(self)
+
+        painter.setRenderHint(QPainter.Antialiasing)
+
+        painter.setPen(Qt.red)
+
+        path.lineTo(20, 12)
+
+        path.lineTo(20, 28)
+
+        path.lineTo(36, 28)
+        path.lineTo(36,12)
+        path.lineTo(20,12)
+        painter.drawPath(path)
+        
 
 def main():
     app = QApplication(sys.argv)
     app.setStyle('Fusion')
     ex = App()
+    ex.showTextbox("Hello")
     sys.exit(app.exec_())
 
 
