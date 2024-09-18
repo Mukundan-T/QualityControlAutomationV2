8ff234d78f4dff589e64a40d62ceff317e6f24e7
libstudent <libstudent@union.edu>
2024-09-18 16:16:03 -0400

    added functionality to autofail for incorrect extent if extent is present on spreadsheet

--- Python Scripts/Preliminary_QC.py
+++ Python Scripts/Preliminary_QC.py
@@ -7,7 +7,7 @@ Edited by:
 
 """
 
-import Files, glob, os
+import Files, glob, os, Excel_reader_writer, re
 from pathlib import Path
 from pdfreader import PDFDocument
 
@@ -34,19 +34,32 @@ def file_checker(ProgramData):
                 File.file_size = os.path.getsize(path) >> 20
                 File.extent = len(os.listdir(File.folderName)) - 1
                 if File.file_size > 300:
-                    File.too_large = True
-
+                    File.too_large = True #Slightly redundant but better for data visualization should it be needed
             if not File.exists:
                 ProgramData.Spreadsheet.dataframes[key].loc[Linenumber, "QC Pass/Fail"] = "Fail"
                 ProgramData.Spreadsheet.dataframes[key].loc[Linenumber, "QC Comments"] = "File cannot be located"
-                print(File.filename + " Could not be located, QC: " + ProgramData.Spreadsheet.dataframes[key]["QC Pass/Fail"][Linenumber])
+                ProgramData.Spreadsheet.dataframes[key].loc[Linenumber, "QC Initials"] = "AUTO"
+                # print(File.filename + " Could not be located, QC: " + ProgramData.Spreadsheet.dataframes[key]["QC Pass/Fail"][Linenumber])
                 failures += 1
             if File.too_large:
                 ProgramData.Spreadsheet.dataframes[key].loc[Linenumber, "QC Pass/Fail"] = "Fail"
                 ProgramData.Spreadsheet.dataframes[key].loc[Linenumber, "QC Comments"] = "File too large for upload"
-                print(File.filename + " Ttoo large for upload, QC: " + ProgramData.Spreadsheet.dataframes[key]["QC Pass/Fail"][Linenumber])
+                ProgramData.Spreadsheet.dataframes[key].loc[Linenumber, "QC Initials"] = "AUTO"
+                # print(File.filename + " Too large for upload, QC: " + ProgramData.Spreadsheet.dataframes[key]["QC Pass/Fail"][Linenumber])
                 failures += 1
+
+            spreadsheet_extent = ProgramData.Spreadsheet.dataframes[key]["extent (total page count including covers)"][Linenumber]
+            try:
+                spreadsheet_extent = int(re.findall(r'\d+', spreadsheet_extent)[0])
+                if File.extent != spreadsheet_extent and File.exists:
+                    ProgramData.Spreadsheet.dataframes[key].loc[Linenumber, "QC Pass/Fail"] = "Fail"
+                    ProgramData.Spreadsheet.dataframes[key].loc[Linenumber, "QC Comments"] = "Extent and pagecount do not match"
+                    ProgramData.Spreadsheet.dataframes[key].loc[Linenumber, "QC Initials"] = "AUTO"
+                    print(File.filename + " Has an incorrect extent, QC: " + ProgramData.Spreadsheet.dataframes[key]["QC Pass/Fail"][Linenumber])
+            except TypeError:
+                spreadsheet_extent = 0
                 
+
             Linenumber += 1
     print("Failure rate: " + str(failures/files * 100) + "%")
 
@@ -56,3 +69,4 @@ def file_checker(ProgramData):
 def run_checks(ProgramData):
     create_possible_files(ProgramData)
     file_checker(ProgramData)
+    Excel_reader_writer.df_to_excel(ProgramData.Spreadsheet.dataframes, ProgramData.Spreadsheet.sheetnames, ProgramData.Spreadsheet.filepath)
