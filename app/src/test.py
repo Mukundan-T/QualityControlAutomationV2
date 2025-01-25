e3b0e3611078cf6b5df6070e828f6215b397bdd6
James Gaskell <jamesgaskell@Jamess-MacBook-Pro.local>
2025-01-25 00:44:16 -0500

    added new tests to check if error rate is working

--- app/src/test.py
+++ app/src/test.py
@@ -8,6 +8,8 @@ spreadsheet.createFileStructure()
 
 sheet_num = 0
 
+"""
+
 #print(spreadsheet)
 #print(spreadsheet.sheetList)
 #print(spreadsheet.sheetList[0].fileList)
@@ -28,9 +30,9 @@ for file in spreadsheet.sheetList[sheet_num].fileList:
     if file.errors['Filename'] == True:
         bad_filenames +=1
 
-#print("found "+ str(date) + " date errors")
-#print("found "+ str(dups) + " duplicate filename errors")
-#print("found " + str(bad_filenames) + " filename errors")
+print("found "+ str(date) + " date errors")
+print("found "+ str(dups) + " duplicate filename errors")
+print("found " + str(bad_filenames) + " filename errors")
 
 #print("found " + str(spreadsheet.sheetList[sheet_num].errors) + " errors in total")
 
@@ -41,4 +43,39 @@ print("total failures accross all sheets: " + str(total_failures))
 total_files = spreadsheet.getTotalFiles()
 print("total files accross all sheets: " + str(total_files))
 
+sheetDict = spreadsheet.sheetList[sheet_num].getSheetErrorDict()
+FileDict = spreadsheet.getFileErrorDict()
+print("")
 
+
+"""
+
+for sheet in spreadsheet.sheetList:
+    spreadsheetChecks.check_date_format(sheet)
+    spreadsheetChecks.check_duplicate_filenames(sheet)
+    spreadsheetChecks.check_location_filename(sheet)
+
+    date = 0
+    dups = 0
+    bad_filenames = 0
+    for file in sheet.fileList:
+        if file.errors['Date'] == True:
+            date +=1
+        if file.errors['DupFilename'] == True:
+            dups +=1
+        if file.errors['Filename'] == True:
+            bad_filenames +=1
+
+    print("found "+ str(date) + " date errors")
+    print("found "+ str(dups) + " duplicate filename errors")
+    print("found " + str(bad_filenames) + " filename errors")
+
+    #print("found " + str(sheet.errors) + " errors in total")
+
+total_errors = spreadsheet.getTotalError()
+print("total errors accross all sheets: " + str(total_errors))
+total_failures = spreadsheet.getTotalFailures()
+print("total failures accross all sheets: " + str(total_failures))
+
+print("error rate: " + str(total_errors/spreadsheet.getTotalFiles() * 100))
+print("failure rate: " + str(total_failures/spreadsheet.getTotalFiles() * 100))
